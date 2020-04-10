0f3edfa76712faacb5cc6e7e32e95244ea82b37d
Matheus Silveira <blanchimaah@gmail.com>
2020-04-10 17:58:00 -0300

    Terceiro Desafio GoStack

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
+
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      title : "God of War 4",
+      owner:  "Kratos & Atreus",
+      techs:  "Leviathan Axe"
+    });
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    /**
+     *  não preciso atribuir retorno nesse caso, só passar o id e depois setar
+     *  o array de repos aqui sem o que eu removi.
+     */
+    await api.delete(`/repositories/${id}`);
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {/* aqui dentro eu tenho que loopar o array de repositorios */}
+          {
+          repositories.map(repository => {
+            return (
+                <li key={repository.id}>
+                  {repository.title}
+                <button onClick={() => handleRemoveRepository(`${repository.id}`)}>
+                  Remover
+                </button>
+                </li>
+            )
+          })
+          }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
